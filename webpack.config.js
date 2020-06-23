e5b360546f599edda392b1e524653c440a515fb8
bill0145 <bill0145@hotmail.com>
2020-06-23 15:56:56 -0500

    webpack plugins added

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+
 module.exports = {
     entry: {
       popup: "./src/popup.jsx"
@@ -19,6 +21,11 @@ module.exports = {
         filename: "popup.html",
         inject: true,
         chunks: ["popup"]
+      }),
+      new CopyPlugin({
+        patterns: [
+          { from: "manifest.json", to: "." }
+        ]
       })
     ],
     mode: "development"
