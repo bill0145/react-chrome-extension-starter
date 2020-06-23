de1b186fd9ce2d786a435795b8a12983ca6d9075
bill0145 <bill0145@hotmail.com>
2020-06-23 16:47:41 -0500

    added TS

--- webpack.config.js
+++ webpack.config.js
@@ -6,12 +6,16 @@ module.exports = {
       popup: "./src/popup.jsx"
     },
     resolve: {
-      extensions: [".jsx", ".js"]
+      extensions: [".tsx", ".ts", ".jsx", ".js"]
     },
     module: {
       rules: [
         {
           test: /\.jsx$/, use:"babel-loader"    
+          
+        },
+        {
+          test: /\.tsx$/, use:"ts-loader"
         }
       ]
     },
